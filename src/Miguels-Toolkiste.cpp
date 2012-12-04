67b303fb1778526126146e397aa969c2ea903039
MiguelGuse <miggi.werder@live.de>
2012-12-04 16:47:06 -0300

    buggy test version;)

--- src/Miguels-Toolkiste.cpp
+++ src/Miguels-Toolkiste.cpp
@@ -14,18 +14,66 @@ int main()
 {
 	string name;
 	int wahl;
+
 	cout << "Hallo, Wie heissen sie?" << endl;                                       //Gruß+Name erfragen
 	cin  >>name;                                                                     //Name einlesen
 	cout <<"Wilkommen in Miguel's Toolkiste, "<<name<<"!!"<<endl;                    //Mit Namen begrüßen
-	cout <<"Was wollen Sie machen?"<<endl;
-	cout <<"1) Taschenrechner"<<endl;                                                //Menü-Option 1) ausgeben
-	cout <<"0)Beenden"<<endl;                                                        //Menü-Option 0)Beenden ausgeben
 	do
 	{
+		cout <<"Was wollen Sie machen?"<<endl;
+			cout <<"1) Taschenrechner"<<endl;                                                //Menü-Option 1) ausgeben
+			cout <<"0)Beenden"<<endl;                                                        //Menü-Option 0)Beenden ausgeben
 		cin  >>wahl;                                                                //Menü-Auswahl einlesen
 		if (wahl==1)
 		{
+			char rechner;                                                          //Fortsetzungsanfrage um später Rechnungen zu wiederholen
+
 			cout <<"Der unfertige Taschenrechner!"<<endl;
+			cout <<"+ zum Addieren \n - zum Subtrahieren \n * zum Multiplizieren \n / zum Dividieren"<<endl;     //Operatoren ausgeben
+			cout <<"Zum Fortfahren beliebigen Buchstaben drücken und anschliessend Rechnung eingeben."<<endl;
+			cout <<"Zum Beenden beliebige Zahl drücken."<<endl;
+
+			while (cin>>rechner)
+			{
+				int wert1;                                                            //Werte für anschliessende Rechnung deklarieren.
+				int wert2;
+				char oprt;                                                              //Operator-Wert( also + - * / )
+				int ergebnis;
+
+				cin >>wert1 >> oprt >> wert2;
+				/* Der Operator wird als char eingelesen, dass heisst wir müssen die ASCII-Werte von +,-,*,/,rausfinden.Google hilft:
+				 *  + entspricht '43'
+				 *  - entspricht '45'
+				 *  * entspricht '42'
+				 *  / entspricht '47'
+				 */
+				if (oprt=='+')                                                                 //Addition
+				{
+					ergebnis= wert1 + wert2;
+					cout <<"= "<< ergebnis<<endl;
+				}
+				else if (oprt=='-')
+				{
+					ergebnis= wert1 - wert2;
+					cout <<"= "<< ergebnis<<endl;
+				}
+				else if (oprt=='*')
+				{
+					ergebnis= wert1 * wert2;
+					cout <<"= "<< ergebnis <<endl;
+				}
+				else if (oprt=='/')
+				{
+					ergebnis= wert1/wert2;
+					cout <<"= "<< ergebnis<<endl;
+				}
+				else
+				{
+					cout <<"Bitte einen gültigen Operanden eingeben!" <<endl;
+				}
+
+
+			}
 		}
 
 	}
